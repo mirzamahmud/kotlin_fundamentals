b6698cd8d6311d35d0fab15a5601fa2555cb87fa
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2024-01-01 11:52:44 +0600

    first_kotlin_syntax

---
+++ 1_hello_world.kt
@@ -0,0 +1,11 @@
+package mirza.dev.kotlinbasics
+
+// TODO -> "fun" is a keyword to declare function/method in Kotlin
+// TODO -> main() -> it is a function/method. All codes execute from here
+fun main(){
+    // TODO -> "println()" this method is used to print expected output with a new line
+    println("Hello, Kotlin. I'm Mirza Mahmud who is passionate to develop software")
+
+    // TODO -> "println()" this method is used to print expected output.
+    print("Mirza Mahmud Hossan")
+}
