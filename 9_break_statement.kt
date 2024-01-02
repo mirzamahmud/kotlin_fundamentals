070f9717fb25cfd69a0323e00c107f5f4b4ff430
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2024-01-02 16:19:52 +0600

    kotlin_break_statement

---
+++ 9_break_statement.kt
@@ -0,0 +1,14 @@
+package mirza.dev.kotlinbasics
+
+// TODO -> "break" statement is used to jump out of a loop.
+
+fun main(){
+    var i : Int = 0;
+    while (i < 5){
+        i++
+        if(i == 3){
+            break
+        }
+        println(i)
+    }
+}
