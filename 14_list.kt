c8e27c962a15acc7c87fc55afcdf92928c32a5ce
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2024-01-05 04:30:57 +0600

    kotlin_list_collection

---
+++ 14_list.kt
@@ -0,0 +1,12 @@
+package mirza.dev.kotlinbasics
+
+// TODO -> Lists store items in the order that they are added, and allow for duplicate items.
+// TODO -> To create read only "list" use the "listOf()" function.
+// TODO -> To create "mutable list" use the "mutableListOf()" function.
+fun main(){
+    // read only list
+    val readOnlyShapes : List<String> = listOf("triangle", "square", "circle")
+    println("Read only shapes: $readOnlyShapes")
+
+
+}
